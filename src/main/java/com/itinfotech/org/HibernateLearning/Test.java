f6e0ebb5c6b4378455fa3439a3565954aa087f2e
sagar <mamodiyasagar@gmail.com>
2018-06-28 07:04:40 +0530

    new Hibernate Learning Project

---
+++ src/main/java/com/itinfotech/org/HibernateLearning/Test.java
@@ -0,0 +1,27 @@
+package com.itinfotech.org.HibernateLearning;
+
+
+
+import java.util.Date;
+ 
+import org.hibernate.Session;
+ 
+public class Test {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+ 
+        session.beginTransaction();
+        User user = new User();
+ 
+        user.setUserId(1);
+        user.setUsername("Mukesh");
+        user.setCreatedBy("Google");
+        //new Date() will initialize with current date and time.
+        user.setCreatedDate(new Date());
+ 
+        session.save(user);
+        session.getTransaction().commit();
+ 
+    }
+ 
+}
